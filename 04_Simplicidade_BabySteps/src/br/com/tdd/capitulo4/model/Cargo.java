32d97034ebb5a306271bb0ccf10858d8e06f531b
Wagner Almeida <wdmeida@gmail.com>
2017-02-20 10:04:30 -0300

    Limpando repositório.

--- 04_Simplicidade_BabySteps/src/br/com/tdd/capitulo4/model/Cargo.java
+++
@@ -1,7 +0,0 @@
-package br.com.tdd.capitulo4.model;
-
-public enum Cargo {
-	DESENVOLVEDOR,
-	DBA,
-	TESTADOR
-}
--- README.md
+++
@@ -1,1 +0,0 @@
-Repositório com os projetos desenvolvidos no livro TDD - Teste e Design no Mundo Real.
