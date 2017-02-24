4b758c8280484e9e8b379bd68db7479e0bfdf06e
Wagner Almeida <wdmeida@gmail.com>
2017-02-24 10:27:23 -0300

    Exemplo do cap 08 (com instodução a Mock).

--- Cap08/src/br/capitulo8/controller/GeradorDeNotaFilscal.java
+++ Cap08/src/br/capitulo8/controller/GeradorDeNotaFilscal.java
@@ -7,12 +7,21 @@ import br.capitulo8.model.NotaFiscal;
 import br.capitulo8.model.Pedido;
 
 public class GeradorDeNotaFilscal {
+	private NFDao dao;
+	private final SAP sap;
+	
+	public GeradorDeNotaFilscal(NFDao dao, SAP sap) {
+		this.dao = dao;
+		this.sap = sap;
+	}
+	
 	public NotaFiscal gera(Pedido pedido) {
 		NotaFiscal nf = new NotaFiscal(pedido.getCliente(),
 				pedido.getValorTotal() * 0.94, 
 				Calendar.getInstance());
 		
-		new NFDao().persiste(nf);
+		dao.persiste(nf);
+		sap.envia(nf);
 		
 		return nf;
 	}
--- Cap08/src/br/capitulo8/test/GeradorNotaFilacalTest.java
+++ Cap08/src/br/capitulo8/test/GeradorNotaFilacalTest.java
@@ -1,22 +1,58 @@
 package br.capitulo8.test;
 
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
+
 import br.capitulo8.controller.GeradorDeNotaFilscal;
+import br.capitulo8.controller.SAP;
+import br.capitulo8.dao.NFDao;
 import br.capitulo8.model.NotaFiscal;
 import br.capitulo8.model.Pedido;
 
-import static org.junit.Assert.*;
-
-import org.junit.Test;
-
 public class GeradorNotaFilacalTest {
+	private NFDao dao;
+	private SAP sap;
+	
+	@Before
+	public void instanciaDependencias(){
+		dao = Mockito.mock(NFDao.class);
+		sap = Mockito.mock(SAP.class);
+	}
 	
 	@Test
 	public void deveGerarNFComValorDeImpostoDescontado(){
-		GeradorDeNotaFilscal gerador = new GeradorDeNotaFilscal();
+		GeradorDeNotaFilscal gerador = new GeradorDeNotaFilscal(dao, sap);
 		Pedido pedido = new Pedido("Wagner",1000, 1);
 		
 		NotaFiscal nf = gerador.gera(pedido);
 		
 		assertEquals(1000 * 0.94, nf.getValor(), 0.00001);
+	}
+	
+	@Test
+	public void devePersistirNFGerada() {
+		// Criando o objeto mock.
+		NFDao dao = Mockito.mock(NFDao.class);
+		
+		GeradorDeNotaFilscal gerador = new GeradorDeNotaFilscal(dao, sap);
+		Pedido pedido = new Pedido("Wagner", 1000, 1);
+		
+		NotaFiscal nf = gerador.gera(pedido);
+		
+		//Verificando que o método foi invocado.
+		Mockito.verify(dao).persiste(nf);
+	}
+	
+	@Test
+	public void deveEnviarNFGeradaParaSAP() {		
+		GeradorDeNotaFilscal gerador = new GeradorDeNotaFilscal(dao, sap);
+		Pedido pedido = new Pedido("Wagner", 1000, 1);
+		
+		NotaFiscal nf = gerador.gera(pedido);
+		
+		Mockito.verify(sap).envia(nf);
 	}
 }
